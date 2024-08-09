c04de794c0069411cf5e60b29fffc9d1ca12624f
defaultnastia <anastayaaa@gmail.com>
2024-08-09 21:31:46 +0200

    updated

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,13 +1,5 @@
 import Joi from "joi";
 
-export const createContactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
+export const createContactSchema = Joi.object({});
 
-export const updateContactSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string(),
-  phone: Joi.string(),
-});
+export const updateContactSchema = Joi.object({});
