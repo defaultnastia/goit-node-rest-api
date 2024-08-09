bf0521fc13d09873579c997871e1659021dde07f
defaultnastia <anastayaaa@gmail.com>
2024-08-09 20:57:40 +0200

    added all controllers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,86 @@
-import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+import * as contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+const getAllContacts = async (_, res, next) => {
+  try {
+    const result = await contactsService.listContacts();
 
-export const getOneContact = (req, res) => {};
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.getContactById(id);
 
-export const createContact = (req, res) => {};
+    if (!result) throw HttpError(404, "Not found");
 
-export const updateContact = (req, res) => {};
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
+
+    if (!result) throw HttpError(404, "Not found");
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createContact = async (req, res, next) => {
+  try {
+    const data = req.body;
+
+    const { error } = createContactSchema.validate(data);
+    if (error) throw HttpError(404, error.message);
+
+    const result = await contactsService.addContact(data);
+
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+
+    if (!Object.keys(data).length)
+      throw HttpError(400, "Body must have at least one field");
+
+    const { error } = updateContactSchema.validate(data);
+    if (error) throw HttpError(400, error.message);
+
+    const result = await contactsService.updateContact(id, data);
+
+    if (!result) throw HttpError(404, "Not found");
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  getAllContacts,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateContact,
+};
