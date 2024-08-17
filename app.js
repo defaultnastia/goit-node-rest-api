3604aacb2843f0d59caa61dc7032906fcfd1c2ca
defaultnastia <anastayaaa@gmail.com>
2024-08-17 14:35:39 +0200

    added dot env

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import contactsRouter from "./routes/contactsRouter.js";
 import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 
