6d1072532d94d25fd13b654d44660057498b9ecd
defaultnastia <anastayaaa@gmail.com>
2024-08-21 12:47:37 +0200

    added auth structure

---
+++ models/User.js
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+import { handleServerError, setUpdateOptions } from "./hooks.js";
+import { emailRegExp } from "../constants/user-constants.js";
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegExp,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false }
+);
+
+export default userSchema;
